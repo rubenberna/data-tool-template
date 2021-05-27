fa1ecf17f302617bd14cdbcd53355d2942659853
rubenberna <rubenmbernardes@gmail.com>
2021-05-27 11:17:59 +0200

    Added fonts and created sidenav

---
+++ example/src/SideNav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const SideNav = () => {
+
+  return (
+    <div className="sidenav">
+      <h3 className="title">Volvo</h3>
+      <h4 className="sub-title">( workbench )</h4>
+    </div>
+  )
+}
